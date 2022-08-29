c1c74e04cbe201aea9bdbe1fc3c87e918b9ef2e9
Tirso Pascual <tpascualgarcia@gmail.com>
2022-08-29 10:17:31 +0900

    try first route successfully

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import express from "express"
 import 'dotenv/config'
 
-
-
 console.log(`\n*\n*\n*\n*\n*\n*\n*\n*\n*
 ******************************************************************`)
 
 const app = express()
-const PORT = 5001
+const PORT = process.env.PORT || 8000
+
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
 
 app.listen(PORT, () => {
   console.log(`Listening at:\nhttp://localhost:${PORT}\n`)
