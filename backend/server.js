129ce8fddad4487209a993cd33b764f7c0c77b57
Tirso Pascual <tpascualgarcia@gmail.com>
2022-08-29 09:26:46 +0900

    set server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("pepe")
+import express from "express"
+import 'dotenv/config'
+
+
+
+console.log(`\n*\n*\n*\n*\n*\n*\n*\n*\n*
+******************************************************************`)
+
+const app = express()
+const PORT = 5001
+
+app.listen(PORT, () => {
+  console.log(`Listening at:\nhttp://localhost:${PORT}\n`)
+})
