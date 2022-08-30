8f2d46f0aaa04d41d75f1b98df15ba8074d4da2b
Tirso Pascual <tpascualgarcia@gmail.com>
2022-08-30 15:23:38 +0900

    add bcrypt when registering a user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,8 +2,11 @@ import asyncHandler from "../middleware/asyncHandler.js"
 import User from "../models/userModel.js"
 
 const users = {
+
+
   registerUser: asyncHandler(async (req, res) => {
     const { name, email, password } = req.body
+
     if (!name || !email || !password) {
       res.status(400)   // Bad Request
       throw new Error("Please include all fields.")
@@ -13,8 +16,18 @@ const users = {
       res.status(400)
       throw new Error("User already exists.")
     }
-    res.send('register route')
+    const user = new User({ name, email, password })
+    await user.save()   // before saving, the password is hashed (see userModel.js)
+
+    res.status(201)   // Created
+    res.json({
+      _id: user._id,
+      name,
+      email
+    })
   }),
+
+
 
   loginUser: asyncHandler(async (req, res) => {
     res.send('login route')
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose"
+import bcrypt from 'bcryptjs'
+
+
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,7 +24,16 @@ const userSchema = new mongoose.Schema({
   },
 },
   {
-    timestamps: true,
+    timestamps: true
   })
+
+
+// cannot pass arrow func as callback; only regular func. 'this' here refers to the instance
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next()
+  this.password = await bcrypt.hash(this.password, 12)
+  next()
+})
+
 
 export default mongoose.model('User', userSchema) 
