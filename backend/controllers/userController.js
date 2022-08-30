0e9f519126905d6adb9dbe5ce52f0ae7d0b0fb59
Tirso Pascual <tpascualgarcia@gmail.com>
2022-08-30 11:20:24 +0900

    add async handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,17 @@
+import asyncHandler from "../middleware/asyncHandler.js"
+
 const users = {
-  registerUser: (req, res) => {
+  registerUser: asyncHandler(async (req, res) => {
     const { name, email, password } = req.body
     if (!name || !email || !password) {
       res.status(400)   // Bad Request
       throw new Error("Please include all fields.")
     }
     res.send('register route')
-  },
-  loginUser: (req, res) => {
+  }),
+  loginUser: asyncHandler(async (req, res) => {
     res.send('login route')
-  }
+  })
 }
 
 export default users
