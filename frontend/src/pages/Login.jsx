3599100e83314c87f42ad37213472957eff471e2
Tirso Pascual <tpascualgarcia@gmail.com>
2022-10-21 20:05:23 +0200

    add login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,65 @@
+import { useState } from "react"
+import { FaSignInAlt } from "react-icons/fa"
+import { toast } from "react-toastify"
 
 export default function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+  }
+
   return (
-    <div>Login</div>
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please log in to create support</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input type="email"
+              className="form-control"
+              id="email"
+              value={email}
+              name='email'
+              onChange={onChange}
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input type="password"
+              className="form-control"
+              id="password"
+              value={password}
+              name='password'
+              onChange={onChange}
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Log in</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
