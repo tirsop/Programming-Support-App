5a996e6055f3669b20347becb668c463f5fcc4c2
Tirso Pascual <tpascualgarcia@gmail.com>
2022-10-21 19:23:18 +0200

    set routes in App.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+// pages
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 function App() {
   return (
-    <div>
-      My App
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   )
 }
 
