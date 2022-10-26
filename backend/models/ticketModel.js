003c9efa776df2943d223eb8c1876513093816a4
Tirso Pascual <tpascualgarcia@gmail.com>
2022-10-26 10:37:58 +0200

    create ticket schema

---
+++ backend/models/ticketModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose"
+
+
+const ticketSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  product: {
+    type: String,
+    required: [true, 'Please select a topic'],
+    enum: ['Formula1', 'Surf', 'JavaScript', 'Japan']
+  },
+  description: {
+    type: String,
+    required: [true, 'Please describe your problem/question']
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ['new', 'open', 'closed'],
+    default: 'new'
+  }
+},
+  {
+    timestamps: true
+  })
+
+export default mongoose.model('Ticket', ticketSchema)
