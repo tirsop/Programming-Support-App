f85abed50fa0383e28a1af67cfe7a87b9bf9420a
Tirso Pascual <tpascualgarcia@gmail.com>
2022-12-07 09:11:00 +0900

    add Note model in mongo

---
+++ backend/models/noteModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose"
+
+const noteSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  ticket: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Ticket'
+  },
+  text: {
+    type: String,
+    required: [true, 'Please add some text']
+  },
+  isStaff: {
+    type: Boolean,
+    default: false
+  },
+  staffId: {
+    type: String
+  }
+},
+  {
+    timestamps: true
+  })
+
+export default mongoose.model('Note', noteSchema)
